a038ace176ade62de6f8cb4b70d88306e15b88fc
Sinh Nguyen <sinh.nt1@urbox.vn>
2024-11-09 20:07:02 +0700

    feat: config eslint and prettier, husky

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
