b52de6af44a48ef59d1c327b91a719cc62254f89
yuka166@github <hoangdat1606@gmail.com>
2024-11-22 01:26:51 +0700

    fix: calendar cell css

--- src/components/custom-calendar/calendar-cell.tsx
+++ src/components/custom-calendar/calendar-cell.tsx
@@ -7,13 +7,13 @@ const CalendarCell: React.FC<Props> = ({ dayInMonth, month }) => {
   return (
     <div className="w-[calc(100%/7)] border rounded text-sm">
       <div className="flex justify-between p-2 border-b">
-        <div>
-          {dayInMonth}/{month}
+        <div className="font-bold">
+          {dayInMonth.toString().padStart(2, "0")}/{month}
         </div>
         <div>09:00 - 18:00</div>
       </div>
       <div className="grid grid-cols-2">
-        <div className="col-start-1 border-b flex justify-center items-center py-4 px-2">
+        <div className="col-start-1 font-bold text-lg border-b flex justify-center items-center py-4 px-2">
           1
         </div>
         <div className="col-start-1 py-4 px-2 flex justify-center items-center">
