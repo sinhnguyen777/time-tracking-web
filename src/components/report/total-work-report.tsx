69821c2894ed38576c2a4bdad8ba35af761229ba
Sinh Nguyen <sinh.nt1@urbox.vn>
2024-11-23 06:41:32 +0700

    feat: statistical in month

--- src/components/report/total-work-report.tsx
+++ src/components/report/total-work-report.tsx
@@ -1,52 +1,51 @@
+import { Typography } from "antd";
+import dayjs from "dayjs";
 import React from "react";
-import { totalWorkReportData } from "./report-data";
 
-const TotalWorkReport = () => {
-  const data = totalWorkReportData;
+interface TotalWorkReportProps {
+  data: any;
+}
 
+const TotalWorkReport: React.FC<TotalWorkReportProps> = ({ data }) => {
   return (
-    <div style={{ margin: "20px", fontSize: "16px", lineHeight: "24px" }}>
-      <p>
-        <strong>Mã NV:</strong> {data.maNV}
-      </p>
-      <p>
-        <strong>Họ và tên:</strong> {data.hoTen}
-      </p>
-      <p>
-        <strong>Vị trí:</strong> {data.viTri}
-      </p>
-      <p>
-        <strong>Công tiêu chuẩn:</strong> {data.congTieuChuan}
-      </p>
-      <p>
-        <strong>Công thực tế:</strong> {data.congThucTe}
-      </p>
-      <p>
-        <strong>Số ngày phép đến tháng 2:</strong> {data.phepDenThang2}
-      </p>
-      <p>
-        <strong>Số ngày phép tháng 3 sử dụng:</strong> {data.phepThang3}
-      </p>
-      <p>
-        <strong>Số ngày phép còn lại:</strong> {data.phepConLai}
-      </p>
-      <p>
-        <strong>Tăng ca:</strong> {data.tangCa}
-      </p>
-      <p>
-        <strong>Làm bù:</strong> {data.lamBu}
-      </p>
-      <p>
-        <strong>Nghỉ phép:</strong> {data.nghiPhep}
-      </p>
-      <p className="pb-3">
-        <strong>Nghỉ không phép:</strong> {data.nghiKhongPhep}
-      </p>
-      <hr />
-      <p style={{ fontWeight: "bold", color: "red" }} className="pt-3">
-        <strong>Tổng công:</strong> {data.tongCong}
-      </p>
-    </div>
+    <>
+      <div style={{ margin: "20px" }}>
+        <Typography.Title level={4}>
+          Báo cáo tổng số công tháng {dayjs().month() + 1}
+        </Typography.Title>
+      </div>
+      <div style={{ margin: "20px", fontSize: "16px", lineHeight: "24px" }}>
+        <p>
+          <strong>Mã NV:</strong> {data.code}
+        </p>
+        <p>
+          <strong>Họ và tên:</strong> {data.name}
+        </p>
+        <p>
+          <strong>Vị trí:</strong> {data.position}
+        </p>
+        <p>
+          <strong>Công tiêu chuẩn:</strong> {data.standardWorkDays}
+        </p>
+        <p>
+          <strong>Tổng số ngày làm việc:</strong> {data.totalWorkingDays}
+        </p>
+        <p>
+          <strong>Tổng số ngày đi trễ:</strong> {data.totalLateDays}
+        </p>
+        <p>
+          <strong>Tổng số ngày nghỉ:</strong> {data.totalAbsentDays}
+        </p>
+        <p>
+          <strong>Tổng số phút trễ:</strong> {data.totalLateMinutes}
+        </p>
+
+        <hr />
+        <p style={{ fontWeight: "bold", color: "red" }} className="pt-3">
+          <strong>Tổng công:</strong> {data.totalDays}
+        </p>
+      </div>
+    </>
   );
 };
 
--- src/pages/report/index.tsx
+++ src/pages/report/index.tsx
@@ -1,5 +1,5 @@
 // pages/report/index.tsx
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReportFilters from "@/components/report/report-filter";
 import LateReportTable from "@/components/report/late-report-table";
 import AttendanceDetailTable from "@/components/report/attendance-detail-table";
@@ -12,9 +12,35 @@ import {
 } from "@/components/report/report-data";
 // import Menu from "@/components/layout/menu";
 import Layout from "@/components/layout";
+import axiosInterceptorInstance from "@/axios/axiosInterceptorInstance";
+import dayjs from "dayjs";
 
-const ReportPage = () => {
-  const [selectedReport, setSelectedReport] = useState("late");
+const ReportPage: React.FC = () => {
+  const [selectedReport, setSelectedReport] = useState("totalWork");
+  const [dataReport, setDataReport] = useState<any[]>([]);
+
+  const currentMonth = dayjs().month();
+  const currentYear = dayjs().year();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const user_id = JSON.parse(localStorage.getItem("user-info") || "{}");
+      try {
+        const response = await axiosInterceptorInstance
+          .get(
+            `/timekeeping/monthly-report/${user_id.id}?month=${currentMonth + 1}&year=${currentYear}`
+          )
+          .then((res) => res.data);
+
+        setDataReport(response.data);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [currentMonth, currentYear]);
 
   return (
     <Layout>
@@ -29,7 +55,7 @@ const ReportPage = () => {
       {selectedReport === "attendance" && (
         <AttendanceDetailTable data={attendanceDetailData} />
       )}
-      {selectedReport === "totalWork" && <TotalWorkReport />}
+      {selectedReport === "totalWork" && <TotalWorkReport data={dataReport} />}
     </Layout>
   );
 };
