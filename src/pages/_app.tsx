54f30feca018f0f85f878e6e8c2a90579b1bf71c
Sinh Nguyen <sinh.nt1@urbox.vn>
2024-11-23 07:20:14 +0700

    fix: expiresIn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
+import { isTokenExpired } from "@/utils/jwt";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      const token = localStorage.getItem("token");
+      if (token && isTokenExpired(token)) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user-info");
+        router.push("/login");
+      }
+    } else {
+      router.push("/login");
+    }
+  }, [router]);
+
   return <Component {...pageProps} />;
 }
